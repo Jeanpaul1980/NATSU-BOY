071515e171068898c87544218a6dcbe06011ed16
Jeanpaul1980 <roodneyjeanpaul0@gmail.com>
2025-08-17 11:18:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const {
   const mode = config.MODE
   const online = config.ALWAYS_ONLINE
   const status = config.AUTO_STATUS_SEEN
-  const ownerNumber = ['243833389567']
+  const ownerNumber = ['50943449848']
   
   const tempDir = path.join(os.tmpdir(), 'cache-temp')
   if (!fs.existsSync(tempDir)) {
